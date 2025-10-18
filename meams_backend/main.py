de11e8d436b189558567d7f8858d8d3cfc9d0620
yelcaole27 <yelcaole27@gmail.com>
2025-10-19 06:49:35 +0800

    Update main.py

--- meams_backend/main.py
+++ meams_backend/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-from .config import API_TITLE, API_VERSION, ALLOWED_ORIGINS
-from . import config
-from .database import connect_db
-from .routers import help_support
+from config import API_TITLE, API_VERSION, ALLOWED_ORIGINS
+from import config
+from database import connect_db
+from routers import help_support
 import time
 app = FastAPI(title=API_TITLE, version=API_VERSION)
 
