47725300b5cec248c899055c551db659ba273ee2
dazztine <148173705+dazztine@users.noreply.github.com>
2025-10-24 15:09:32 +0800

    Update link in MainLayout to point to datatransfer

--- src/MainLayout.js
+++ src/MainLayout.js
@@ -397,7 +397,7 @@ function MainLayout() {
               </Link>
             </li>
             <li>
-              <Link to="/settings" className={`nav-item ${isActive('/settings') ? 'active' : ''}`}>
+              <Link to="/datatransfer" className={`nav-item ${isActive('/datatransfer') ? 'active' : ''}`}>
                 <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path d="M14 2H6C4.89543 2 4 2.89543 4 4V20C4 21.1046 4.89543 22 6 22H18C19.1046 22 20 21.1046 20 20V8L14 2Z" stroke="currentColor" strokeWidth="2" />
                   <path d="M14 2V8H20" stroke="currentColor" strokeWidth="2" />
