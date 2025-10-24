d6f089e5a23169eee2b39a2101dcd10b2a5f6725
dazztine <148173705+dazztine@users.noreply.github.com>
2025-10-24 14:03:05 +0800

    Remove update comment from index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// index.js - Update your existing index.js file
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
